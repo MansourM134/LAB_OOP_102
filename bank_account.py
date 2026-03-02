094b29a47f46044a85a3f1ab6d0a2a333d196334
mansour134 <mansourmasood134@gmail.com>
2026-03-02 09:15:49 +0300

    solution

---
+++ bank_account.py
@@ -0,0 +1,45 @@
+'''
+
+Create a Python class called `BankAccount` that simulates a simple bank account. The class should have the following functionalities:
+
+1. It should have a constructor that accepts the `account_holder` name and initial balance (`initial_balance`), setting the balance to zero if the initial balance is not provided.
+2. A method called `deposit` that accepts an amount and adds it to the account balance, and then returns the updated balance.
+3. A method called `withdraw` that accepts an amount and subtracts it from the account balance, returning the updated balance, but only if there are sufficient funds in the account. If there are insufficient funds, it should raise an Exception and leave the balance unchanged (make sure to handle the exception when calling the method).
+4. A method called `get_balance` that returns the current account balance.
+5. A method called `get_account_holder` that returns the name of the account holder.
+
+**NOTE:**
+- Do validation, and other OOP principles as needed. 
+- arrange your code in at least 2 files.
+'''
+
+class BankAccount:
+    def __init__(self, account_holder:str, initial_balance:float = 0):
+       
+        self._account_holder = account_holder
+        self._initial_balance = initial_balance
+
+    def deposit(self, amount:float):
+
+        self._initial_balance += amount
+        return self._initial_balance
+    
+    def withdraw(self, amount:float):
+
+
+        if amount < 0:
+            raise ValueError("amount cant be negative")
+        if amount > self._initial_balance:
+            raise Exception("you dont have the funds")
+
+        self._initial_balance -= amount
+        return self._initial_balance
+    
+    def get_balance(self):
+        return self._initial_balance
+    def get_account_holder(self):
+        return self._account_holder 
+
+
+
+        pass
---
+++ main.py
@@ -0,0 +1,23 @@
+from bank_account import BankAccount
+
+def main():
+    account = BankAccount("Mansour", 999)
+
+    account.deposit(1)
+    print(f" your account has: {account.get_balance}")
+
+
+    try:
+        account.withdraw(1001)
+        print(f" your account has: {account.get_balance}")
+    except Exception as e:
+        print(f"withrdaw denied {e}")
+
+    
+    print(f"this account belongs to {account.get_account_holder}")
+    print(f"you current balance is {account.get_balance}")
+
+    
+main()
+
+    
