bbd86999c2defc6434584ed93d229c827a9daaea
mansour134 <mansourmasood134@gmail.com>
2026-03-02 12:25:27 +0300

    solution

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ def main():
     account = BankAccount("Mansour", 999)
 
     account.deposit(1)
-    print(f" your account has: {account.get_balance}")
+    print(f" your account has: {account.get_balance()}")
 
 
     try:
         account.withdraw(1001)
-        print(f" your account has: {account.get_balance}")
+        print(f" your account has: {account.get_balance()}")
     except Exception as e:
         print(f"withrdaw denied {e}")
 
     
-    print(f"this account belongs to {account.get_account_holder}")
-    print(f"you current balance is {account.get_balance}")
+    print(f"this account belongs to {account.get_account_holder()}")
+    print(f"you current balance is {account.get_balance()}")
 
     
 main()
